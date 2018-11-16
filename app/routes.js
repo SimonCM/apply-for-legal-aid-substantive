29901475e4dac85ff0ebfdf3c63041b394d3f5e0
Simon Meredith <simoncmeredith@gmail.com>
2018-11-16 12:30:50 +0000

    New repo

--- app/routes.js
+++ app/routes.js
@@ -7,10 +7,6 @@ router.get('/', function (req, res) {
   res.render('index')
 })
 
-// Add your routes here - above the module.exports line
-
-module.exports = router
-
 // Route for search proceeding
 
 router.get('/search', function (req, res) {
@@ -21,3 +17,33 @@ router.get('/search', function (req, res) {
     })
 
 })
+
+
+
+// Branching
+router.get('/capital', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+
+  if (req.session.data.capital === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/property')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('capital')
+  }
+})
+
+router.get('/other-capital', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+
+  if (req.session.data.othercapital === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/capital')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('other-capital')
+  }
+})
+
+// Add your routes here - above the module.exports line
+module.exports = router
