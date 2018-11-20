453b87d93653bbbedf02e36d9dbf87ec32586380
Simon Meredith <simoncmeredith@gmail.com>
2018-11-20 14:44:22 +0000

    Lots of changes to flow

--- app/routes.js
+++ app/routes.js
@@ -24,24 +24,36 @@ router.get('/search', function (req, res) {
 router.get('/capital', function (req, res) {
   // Get the answer from the query string (eg. ?over18=false)
 
-  if (req.session.data.capital === 'yes') {
+  if (req.session.data.property === 'yes') {
     // Redirect to the relevant page
-    res.redirect('/property')
+    res.redirect('/yes_holding_page')
   } else {
     // If over18 is any other value (or is missing) render the page requested
     res.render('capital')
   }
 })
 
-router.get('/other-capital', function (req, res) {
+router.get('/other_capital', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+
+  if (req.session.data.capital === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/yes_holding_page')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('other_capital')
+  }
+})
+
+router.get('/other_capital', function (req, res) {
   // Get the answer from the query string (eg. ?over18=false)
 
   if (req.session.data.othercapital === 'yes') {
     // Redirect to the relevant page
-    res.redirect('/capital')
+    res.redirect('/yes_holding_page')
   } else {
     // If over18 is any other value (or is missing) render the page requested
-    res.render('other-capital')
+    res.render('other_capital')
   }
 })
 
